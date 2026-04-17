d6512a3645c253408742a9408a417e3fc4fdbf33
Suriel Francisco <surielf39@gmail.com>
2026-04-16 23:23:18 -0600

    setup production config with SSL

--- database/config/config.js
+++ database/config/config.js
@@ -1,14 +1,24 @@
 require('dotenv').config()
 
 module.exports = {
-	development: {  
-    	url: process.env.DEV_DATABASE_URL, 
-    	dialect: 'postgres', 
+	development: {
+		url: process.env.DEV_DATABASE_URL,
+		dialect: 'postgres',
 		dialectOptions: {
-      		ssl: {
-        		require: true,
-        		rejectUnauthorized: false
-      }
-    }
-  	},
+			ssl: {
+				require: true,
+				rejectUnauthorized: false
+			}
+		}
+	},
+	production: {
+		url: process.env.DATABASE_URL, 
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false
+			}
+		}
+	}
 }
